d7dbe06fc190143a15478f54bb9fa30f11014e02
Tarcontar <michael-schnabel@gmx.net>
2018-09-04 19:40:13 +0200

    first work on graphicsState

---
+++ build/src/core/graphicsState.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+namespace ps
+{
+	class graphicsState final
+	{
+	public:
+		enum LineCap
+		{
+			butt = 0,
+			round = 1,
+			square = 2
+		};
+
+		enum LineJoin
+		{
+			miter = 0,
+			round = 1,
+			bevel = 2
+		};
+
+		enum ColorSpaces
+		{
+			//language level2
+			DeviceGray,
+			DeviceRGB,
+			DeviceCMYK,
+			CIEBasedABC,
+			CIEBasedA,
+			Pattern,
+			Indexed,
+			Separation,
+
+			//language level 3
+			CIEBasedDEF,
+			CIEBasedDEFG,
+			DeviceN
+		};
+
+		struct Color
+		{
+			int R;
+			int G;
+			int B;
+
+			int hue;
+			int saturation;
+			int brightness;
+
+			int cyan;
+			int magenta;
+			int yellow;
+			int black;
+		};
+
+		struct gState
+		{
+			int lineWidth;
+			LineCap lineCap;
+			LineJoin lineJoin;
+			int miterLimit;
+			bool strokeAdjust;
+			ColorSpace cspace;
+			Color color;
+		};
+
+		gState _state;
+	};
+}
+
+
